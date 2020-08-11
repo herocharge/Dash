a53380e671389fcd8fd8c19fc3dd4d26b3d35e9d
herocharge <66515741+herocharge@users.noreply.github.com>
2020-08-11 14:44:10 +0530

    Supplements to 9d2f95f

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 var f= new vector(0,0.01); 
 var box = new createObject("box",100,100,0,10,0,0);
-spawn(box, new coord(0,100));
-var box2 = new createObject("box2",10,10,0,10,0,0);
-spawn(box2, new coord(0,100));
-box.angularVelocity=10;
-// [var box2 = new createObject("box2",10,10,0,10,0,0);
-// spawn(box2, new coord(0,10));
-// var box3 = new createObject("box3",10,10,0,10,0,0);
-// spawn(box3, new coord(0,10));
-// var box4 = new createObject("box4",10,10,0,10,0,0);
-// spawn(box4, new coord(0,10));
-// var box5 = new createObject("box5",10,10,0,10,0,0);
-// spawn(box5, new coord(0,10));]
-// cameraFollow(box);
+spawn(box, new vector(900,500));
+var box2 = new createObject("box2",100,100,0,10,0,0);
+spawn(box2, new vector(600,500));
+box.radius=100;
+box2.radius=100;
+var box3 = new createObject("box",100,100,0,10,0,0);
+spawn(box3, new vector(100,500));
+
+// box.angularVelocity=10;
+
+box.velocity.x=-1;
+box2.velocity.x=10;
 function update(){ 
-    // addForce(box,f);
-    // addTorque(box,100);
-    box2.position=new coord(box.vertex[0].x,box.vertex[0].y);
-    showAxis();
+        // addForce(box2,new vector(0.1,0));
+        box.velocity=new vector(0,0);
+        box.velocity=new vector(0,0);
+        // addForce(box,new vector(0,0.01));
 }
